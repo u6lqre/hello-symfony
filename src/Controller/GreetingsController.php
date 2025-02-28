8468a6d54ab478d105e7b24f48d8e7dbfa94fcf6
u6lqre <ozwtn9@tutamail.com>
2025-02-28 19:27:03 +0100

    add HelloWorld route and GreetingsController

---
+++ src/Controller/GreetingsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GreetingsController extends AbstractController
+{
+  #[Route('/helloworld')]
+  public function show(): Response
+  {
+    return $this->render("hola mundo");
+  }
+}
