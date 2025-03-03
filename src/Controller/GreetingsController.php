2f71724e599388523900f9934b8056aff37c542f
u6lqre <ozwtn9@tutamail.com>
2025-03-03 21:08:39 +0100

    add formatDate function

--- src/Controller/GreetingsController.php
+++ src/Controller/GreetingsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PageAccessRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +17,7 @@ class GreetingsController extends AbstractController
   {
     try {
       $lastAccess = $repository->getLastAccess();
+      $formattedDate = $this->formatDate($lastAccess);
     } catch (Exception $e) {
       $lastAccess = null;
     }
@@ -23,7 +25,34 @@ class GreetingsController extends AbstractController
     $repository->createNewAccess($entityManager);
 
     return $this->render("greetings/show.html.twig", [
-      "lastAccess" => $lastAccess
+      "formattedDate" => $formattedDate
     ]);
+  }
+
+  private function formatDate($lastAccess)
+  {
+    $dateTime = new DateTime($lastAccess);
+
+    $months = [
+      "01" => "enero",
+      "02" => "febrero",
+      "03" => "marzo",
+      "04" => "abril",
+      "05" => "mayo",
+      "06" => "junio",
+      "07" => "julio",
+      "08" => "agosto",
+      "09" => "septiembre",
+      "10" => "octubre",
+      "11" => "noviembre",
+      "12" => "diciembre"
+    ];
+
+    return [
+      'year' => $dateTime->format('Y'),
+      'month' => $months[$dateTime->format('m')],
+      'day' => $dateTime->format('d'),
+      'hour' => $dateTime->format('H:i:s')
+    ];
   }
 }
